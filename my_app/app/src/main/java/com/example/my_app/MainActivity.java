7444b5b03561c5ace34d69f4ef745433011ba639
Sunless <arto.root0@gmail.com>
2025-10-17 14:21:21 +0300

    lab2

--- my_app/app/src/main/java/com/example/my_app/MainActivity.java
+++ my_app/app/src/main/java/com/example/my_app/MainActivity.java
@@ -3,37 +3,43 @@ package com.example.my_app;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity
-implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView textView1, textView2;
+    Button buttonAdd, buttonCopy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn1 = (Button)findViewById(R.id.button7);
-        Button btn2 = (Button)findViewById(R.id.button8);
-        Button btn3 = (Button)findViewById(R.id.button9);
-        Button btn4 = (Button)findViewById(R.id.button10);
 
-        btn1.setOnClickListener(this);
-        btn2.setOnClickListener(this);
-        btn3.setOnClickListener(this);
-        btn4.setOnClickListener(this);
+        textView1 = findViewById(R.id.tv1);
+        textView2 = findViewById(R.id.tv2);
+        buttonAdd = findViewById(R.id.buttonAdd);
+        buttonCopy = findViewById(R.id.buttonCopy);
 
+
+        buttonAdd.setOnClickListener(this);
+        buttonCopy.setOnClickListener(this);
     }
+
     @Override
     public void onClick(View v) {
-        Button btn = (Button)v;
+        int id = v.getId();
 
-        btn.setText(R.string.AfterClick);
+        if (id == R.id.buttonAdd) {
 
+            String s = textView1.getText().toString();
+            textView1.setText(s + "*");
+        }
+        else if (id == R.id.buttonCopy) {
+
+            textView2.setText(textView1.getText().toString());
+        }
     }
 }
