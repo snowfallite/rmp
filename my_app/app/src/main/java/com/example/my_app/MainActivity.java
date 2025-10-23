ab03bf376ff04c8bc4f1d60e370137bcaf4fcddc
Sunless <arto.root0@gmail.com>
2025-10-23 14:31:35 +0300

    lab10

--- my_app/app/src/main/java/com/example/my_app/MainActivity.java
+++ my_app/app/src/main/java/com/example/my_app/MainActivity.java
@@ -1,19 +1,24 @@
 package com.example.my_app;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.widget.ListView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void openSecondActivity(View view) {
-        Intent intent = new Intent(this, SecondActivity.class);
-        startActivity(intent);
+        // получаем ссылку на ListView
+        ListView listView = findViewById(R.id.listView);
+
+        // загружаем массив из ресурсов
+        String[] items = getResources().getStringArray(R.array.items_array);
+
+        // создаём и устанавливаем адаптер
+        ColorsAdapter adapter = new ColorsAdapter(this, items);
+        listView.setAdapter(adapter);
     }
 }
